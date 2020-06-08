d4d4871ded013c6e92e4357020d0979d3682a8c3
dwinurcahyo <66015798+dwinurcahyo@users.noreply.github.com>
2020-06-09 00:03:55 +0700

    Add files via upload

---
+++ assets/quotes.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+export default [
+    { author: "Secret Admirer", quotes: "Whatever happens, keep breathing" },
+    { author: "Secret Admirer", quotes: "Kuliah yang bener" },
+    { author: "Secret Admirer", quotes: "You're my laughter and the reason people ask me why i'm smiling when i don't even realize i am" },
+    { author: "Secret Admirer", quotes: "Jaga kesehatan ya" },
+    { author: "Secret Admirer", quotes: "I realized i was thinking of you , and i began to wonder how long you will been on my mind . Then it occurred to me : Since I met you , you,ve never left" },
+    { author: "Secret Admirer", quotes: "Sukses terus ya" },
+    { author: "Secret Admirer", quotes: "Salam buat orang tua lo ya hehe" },
+];
---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Babe" , "Princess" , "Sweet Heart"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Have A Nice Day",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
